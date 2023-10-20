cc6ba779d6826931f5e3bad3956243cc07473b76
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-10-19 20:42:53 -0600

    ✨ Proyecto Estructurado

--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,7 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './styles/style.css'
+import javascriptLogo from './assets/javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -21,4 +20,3 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
