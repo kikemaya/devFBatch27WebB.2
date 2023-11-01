ce07cd09893ed802b875c3cba66ae465e7844957
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-10-31 21:40:04 -0600

    ✨ Plantilla de landing page base añadida

---
+++ 02-react-props/src/App.jsx
@@ -0,0 +1,120 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <header className='hero'>
+        <div className='textos-hero'>
+          <h1>Bienvenido a Website</h1>
+          <p>Creamos el mejor sitio web para ti</p>
+          <a href='#contacto'>Contactame</a>
+        </div>
+        <div className='svg-hero' style={{ height: '150px', overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150' preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#fff' }}
+            />
+          </svg>
+        </div>
+      </header>
+
+      <section className='wave-contenedor website'>
+        <img src='img/ilustracion1.svg' alt='' />
+        <div className='contenedor-textos-main'>
+          <h2 className='titulo left'>Title of section</h2>
+          <p className='parrafo'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt enim reiciendis
+            molestias nam tempore. Ullam hic accusantium eligendi ipsam corrupti!
+          </p>
+          <a href='' className='cta'>Learn more</a>
+        </div>
+      </section>
+
+      <section className='info'>
+        <div className='contenedor'>
+          <h2 className='titulo left'>Juntos podemos apoyar</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </section>
+
+      <section className='cards contenedor'>
+        <h2 className='titulo'>Our services</h2>
+        <div className='content-cards'>
+          <article className='card'>
+            <i className='far fa-clone' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+          <article className='card'>
+            <i className='fas fa-database' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+          <article className='card'>
+            <i className='far fa-object-group' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>Learn more</a>
+          </article>
+        </div>
+      </section>
+
+      <section className='galeria'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Our work</h2>
+          <article className='galeria-cont'>
+            <img src='img/uno.jpg' alt='' />
+            <img src='img/dos.jpg' alt='' />
+            <img src='img/tres.jpg' alt='' />
+            <img src='img/cuatro.jpg' alt='' />
+            <img src='img/cinco.jpg' alt='' />
+            <img src='img/seis.jpg' alt='' />
+          </article>
+        </div>
+      </section>
+
+      <section className='info-last'>
+
+        <div className='contenedor last-section'>
+          <div className='contenedor-textos-main'>
+            <h2 className='titulo left'>Title of section</h2>
+            <p className='parrafo'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum reprehenderit nostrum expedita quasi odio architecto laudantium sunt nemo dicta atque?</p>
+            <a href='' className='cta'>Learn more</a>
+          </div>
+          <img src='img/ilustracion.svg' alt='' />
+        </div>
+
+        <div className='svg-wave' style={{ height: '150px', overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150' preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#f5576c' }}
+            />
+          </svg>
+        </div>
+      </section>
+
+      <footer id='contacto'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Contact us</h2>
+          <form action='' className='form'>
+            <input className='input' type='text' name='' id='' placeholder='Nombre' />
+            <input className='input' type='email' name='' id='' placeholder='Email' />
+            <textarea className='input' name='' id='' cols='30' rows='10' placeholder='Mensaje' />
+            <input className='input' type='submit' value='Enviar' />
+          </form>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ 02-react-props/src/components/Saludar.jsx
@@ -0,0 +1,9 @@
+const Saludar = ({ nombre, apellido }) => {
+  return (
+    <div>
+      <h1>Hola {nombre} {apellido} </h1>
+    </div>
+  )
+}
+
+export default Saludar
